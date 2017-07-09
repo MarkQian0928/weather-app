ff023640ea309d8e3dc04d9cd8ad4a0fd16f1495
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-09 12:28:13 -0600

    complete course 31, use encode to change address dynamically

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-const request = require('request');
 //request 用来 make http calls
+const request = require('request');
+const yargs = require('yargs');
+
+const input = yargs.options({
+    address:{
+        demand: true, //在terminal里输入的时候，必须要有这个
+        alias: 'a',
+        describe: 'address to fetch weather for',
+        string: true //ask yargs always to parse the address as a string. 'true' make sure we will get the data, not blank
+    }
+    
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+//console.log(input);
+var encodeAddress = encodeURIComponent(input.address);
+
+
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=16712%2015%20avenue%20sw%20edmonton%20canada',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
     json: true,
     /*this tells the request that the data coming back is json data, 
     and it should take the json stirng and converted it to object for us.*/
 }, (error, response, body)=>{
     //print the HTML for the url address. Course 28
-    console.log(body);
+    //console.log(body);
     //course 29, show all info in JSON from url address
-    console.log(JSON.stringify(body, undefined,2)); 
+    //console.log(JSON.stringify(body, undefined,2)); 
     //output only oneline of address, output the specific code that you need
     console.log(`Address: ${body.results[0].formatted_address}`);
     console.log(`latitude: ${body.results[0].geometry.location.lat}`)
