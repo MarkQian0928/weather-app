f00c067bfd3dbd3367f5c5d1f2a0c99d3c2d8ebd
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-09 13:22:02 -0600

    complete course 33. clean up app.js by creating a new file geocode.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
+
 const input = yargs.options({
     address:{
         demand: true, //在terminal里输入的时候，必须要有这个
@@ -15,29 +17,13 @@ const input = yargs.options({
 .alias('help', 'h')
 .argv;
 
-//console.log(input);
-var encodeAddress = encodeURIComponent(input.address);
-
-
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
-    json: true,
-    /*this tells the request that the data coming back is json data, 
-    and it should take the json stirng and converted it to object for us.*/
-}, (error, response, body)=>{
-    if(error){
-        console.log('unbale to connect to the service');
-    }else if(body.status ==="ZERO_RESULTS"){
-        console.log('unable to find that address');
-    }else if(body.status ==='OK'){
-        //print the HTML for the url address. Course 28
-        //console.log(body);
-        //course 29, show all info in JSON from url address
-        //console.log(JSON.stringify(body, undefined,2)); 
-        //output only oneline of address, output the specific code that you need
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`latitude: ${body.results[0].geometry.location.lat}`)
+//在geocode文件里，geocodeAddress = (address, callback)， input.address 等于address; (errowMessage, result)=callback
+geocode.geocodeAddress(input.address,(errowMessage, result)=>{
+    if(errowMessage){
+        console.log(errowMessage);
     }
-    
+    else{
+        console.log(JSON.stringify(result, undefined,2));
+    }
 });
+
---
+++ geocode/geocode.js
@@ -0,0 +1,34 @@
+//request 用来 make http calls
+const request = require('request');
+
+var geocodeAddress = (address, callback)=>{
+    var encodeAddress = encodeURIComponent(address);
+    request({
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
+        json: true,
+        /*this tells the request that the data coming back is json data, 
+        and it should take the json stirng and converted it to object for us.*/
+    }, (error, response, body)=>{
+        if(error){
+            callback('unbale to connect to the service');
+        }else if(body.status ==="ZERO_RESULTS"){
+            callback('unable to find that address');
+        }else if(body.status ==='OK'){
+            //print the HTML for the url address. Course 28
+            //console.log(body);
+            //course 29, show all info in JSON from url address
+            //console.log(JSON.stringify(body, undefined,2)); 
+            //这里的callback 等于app.js里面geocodeAddress里的(errowMessage, result)，undefined代表的是没有errowMessage
+            callback(undefined,{
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat
+            })
+            //output only oneline of address, output the specific code that you need
+            //console.log(`Address: ${body.results[0].formatted_address}`);
+            //console.log(`latitude: ${body.results[0].geometry.location.lat}`)
+        }
+        
+    });
+    };
+
+module.exports.geocodeAddress = geocodeAddress;
