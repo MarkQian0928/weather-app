ee0a865470da4730fa71deb98e9079c24690980d
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-09 12:48:53 -0600

    complete course 32. Solve the callback wrong address issue by using  statements.

--- app.js
+++ app.js
@@ -26,11 +26,18 @@ request({
     /*this tells the request that the data coming back is json data, 
     and it should take the json stirng and converted it to object for us.*/
 }, (error, response, body)=>{
-    //print the HTML for the url address. Course 28
-    console.log(body);
-    //course 29, show all info in JSON from url address
-    console.log(JSON.stringify(body, undefined,2)); 
-    //output only oneline of address, output the specific code that you need
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`latitude: ${body.results[0].geometry.location.lat}`)
+    if(error){
+        console.log('unbale to connect to the service');
+    }else if(body.status ==="ZERO_RESULTS"){
+        console.log('unable to find that address');
+    }else if(body.status ==='OK'){
+        //print the HTML for the url address. Course 28
+        //console.log(body);
+        //course 29, show all info in JSON from url address
+        //console.log(JSON.stringify(body, undefined,2)); 
+        //output only oneline of address, output the specific code that you need
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`latitude: ${body.results[0].geometry.location.lat}`)
+    }
+    
 });
