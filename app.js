21e768df2319f054c885066756a32c29039df0ca
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-09 11:45:22 -0600

    complete course 30, output the specific code

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ request({
     /*this tells the request that the data coming back is json data, 
     and it should take the json stirng and converted it to object for us.*/
 }, (error, response, body)=>{
-    console.log(body);//pritn the HTML for the url address. Course 28
-    console.log(JSON.stringify(body, undefined,2)); //course 29, show all info in JSON from url address
+    //print the HTML for the url address. Course 28
+    console.log(body);
+    //course 29, show all info in JSON from url address
+    console.log(JSON.stringify(body, undefined,2)); 
+    //output only oneline of address, output the specific code that you need
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`latitude: ${body.results[0].geometry.location.lat}`)
 });
