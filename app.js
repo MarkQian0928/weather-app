b191aba5c734dceac2ed881af2984ff38bd66ef0
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-09 11:13:10 -0600

    add some comment

--- app.js
+++ app.js
@@ -7,5 +7,6 @@ request({
     /*this tells the request that the data coming back is json data, 
     and it should take the json stirng and converted it to object for us.*/
 }, (error, response, body)=>{
-    console.log(JSON.stringify(body, undefined,2));//pritn the HTML for the url address.
+    console.log(body);//pritn the HTML for the url address. Course 28
+    console.log(JSON.stringify(body, undefined,2)); //course 29, show all info in JSON from url address
 });
