56096f45fd255fef37940480fd491e46ca6d904e
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-09 09:31:46 -0600

    complete course 28. First try request

---
+++ app.js
@@ -0,0 +1,11 @@
+const request = require('request');
+//request 用来 make http calls
+
+request({
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=16712%2015%20avenue%20sw%20edmonton%20canada',
+    json: true,
+    /*this tells the request that the data coming back is json data, 
+    and it should take the json stirng and converted it to object for us.*/
+}, (error, response, body)=>{
+    console.log(body);//pritn the HTML for the url address.
+});
