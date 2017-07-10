a13229fab07ca2caab8533b93f2a54dbc928a52b
TIANJIAO QIAN <markqian199413@gmail.com>
2017-07-09 22:23:48 -0600

    complete course 36. Tested the reject for Promise

--- playground/promise.js
+++ playground/promise.js
@@ -1,9 +1,12 @@
 var somePromise = new Promise((resolve, reject)=>{
     setTimeout(()=>{
-        resolve('it works');
+        //resolve('it works');
+        reject ('it does not work');
     },2500);
 });
 
 somePromise.then((message)=>{
     console.log(message);
+},(err)=>{
+    console.log(err);
 })
